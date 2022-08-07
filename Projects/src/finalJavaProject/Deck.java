ca6fd885eb998c31e67daca8e451a0afee5ad300
Aaron <steiger154@gmail.com>
2022-08-06 21:32:18 -0600

    Final project

---
+++ Projects/src/finalJavaProject/Deck.java
@@ -0,0 +1,38 @@
+package finalJavaProject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+
+    /*
+     * ********************
+     * Step 1
+     * ********************
+     */
+    List<Card> cards = new ArrayList<Card>();
+    List<String> suits = Arrays.asList("Diamonds", "Hearts", "Clubs", "Spades");
+
+    public Deck() {
+
+        for (int i = 2; i < 15; i++) {
+
+            for (String suit : suits) {
+                cards.add(new Card(i, suit));
+            }
+
+        }
+
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card draw() {
+        return cards.remove(0);
+    }
+
+}
